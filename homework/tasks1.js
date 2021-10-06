360d04214167c2947c639985fe2e06179c6d2e64
Arash4567 <shomoyxhacker007@mail.ru>
2021-10-06 10:41:34 +0500

    homework task1-2-3

--- homework/tasks1.js
+++ homework/tasks1.js
@@ -2,3 +2,13 @@ function task1(x){
     return x**3;
 }
 // console.log(`Kub hajmi: ${task1(15)}`);
+function task2(r1,r2,r3){
+    return Array(Math.PI*r1**2, Math.PI*r2**2, Math.PI*r3**2);
+}
+// console.log(`Doira yuzlari: ${task2(15,2,5)}`);
+
+function task3(s,h){
+    return (2*s)/h;
+}
+// console.log(`Uchburchak asosi: ${task3(15,2)}`);
+
