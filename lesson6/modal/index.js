3c70376358397a0863b52ae3fc45700946557d45
Arash4567 <shomoyxhacker007@mail.ru>
2021-10-12 12:45:45 +0300

    optimize modal

--- lesson6/modal/index.js
+++ lesson6/modal/index.js
@@ -1,21 +1,42 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(modal.classList);
-for (let i = 0; i < btnsShowModal.length; i++){
-    btnsShowModal[i].addEventListener('click', function (){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
+console.log(modal);
+function open() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
-btnCloseModal.addEventListener('click', function (){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", function () {
+      open();
+  });
+}
+function close() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+btnCloseModal.addEventListener("click", function () {
+  close();
 });
-overlay.addEventListener('click', function (){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');    
+overlay.addEventListener("click", function () {
+  close();
 });
+// let person = {
+//     name: "Bob",
+//     age: 13
+// }
+document.addEventListener("keyup", function (klavish) {
+  // console.log(klavish.key);
+  if (klavish.key === "Escape") {
+    close();
+  }
+  if (klavish.key === "Enter") {
+      open();
+  }
+});
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// });
 // TODO Codeni optimallashtiring
 // TODO Esc klavishini bosganda modalni yoping!
