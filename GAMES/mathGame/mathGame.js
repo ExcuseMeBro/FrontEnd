1069b82645bca9f0e8a0645b59b31018ead12f58
Arash4567 <shomoyxhacker007@mail.ru>
2021-11-05 17:18:01 +0300

    GAME

---
+++ GAMES/mathGame/mathGame.js
@@ -0,0 +1,1 @@
+/* Yo`riqnomani link orqali olasizla: https://play.google.com/store/apps/details?id=com.GamesForKids.Mathgames.MultiplicationTables */
---
+++ GAMES/sezar/sezar.js
@@ -0,0 +1,47 @@
+/*# Sezar shifrlash usuli (caesar cipher)
+logo = `
+      ____                              ____ _       _               
+     / ___|__ _  ___  ___  __ _ _ __   / ___(_)_ __ | |__   ___ _ __ 
+    | |   / _` |/ _ \/ __|/ _` | '__| | |   | | '_ \| '_ \ / _ \ '__|
+    | |__| (_| |  __/\__ \ (_| | |    | |___| | |_) | | | |  __/ |   
+     \____\__,_|\___||___/\__,_|_|     \____|_| .__/|_| |_|\___|_|   
+                                              |_|  
+    `
+# STAGE 1 ENCRYPT TEXT
+# TODO 1 Logoni chiqarish
+
+# TODO 2 alphabet nomli list yaratish [Listda 2 marta alphabet harflarini kiriting sababini so`rang!]
+
+# TODO 3 User kiritgan so`zni o`zgaruvchiga yuklash
+
+# TODO 4 Shift [alphabetni nechta qadam bilan surishni o`zgaruvchiga yuklash]
+
+# TODO 5 encrypt nomli funksiya yaratish [funksiya 2 ta parametr qabul qilsin]
+
+# TODO 6 For loopi yordamida user kiritgan so`zni harf birma bir olish
+
+# TODO 7 Har bir harfni alphabet nomli listdagi o`rnini aniqlab position nomli o`zgaruvchiga yuklash
+
+# TODO 8 Loop har bir marta aylanganda position ga shift ni qo`shib natijani encrypted_pos nomli o`zgaruvchiga yuklash
+
+# TODO 9 encrypt nomli funksiyada loopdan tashqarida ya'ni yuqorida encrypted_word nomli bo`sh string yaratish
+
+# TODO 10 8-banddan keyin 9-bandda yaratgan stringga alphabet nomli listning encrypted_pos da turgan elementini qo`shib chiqish
+
+# STAGE 2 DECRYPT TEXT
+
+# TODO 1 decrypt nomli funksiya yaratish [funksiya 2 ta parametr qabul qilsin]
+
+# TODO 2 For loopi yordamida user kiritgan so`zni harf birma bir olish
+
+# TODO 3 Har bir harfni alphabet nomli listdagi o`rnini aniqlab position nomli o`zgaruvchiga yuklash
+
+# TODO 4 Loop har bir marta aylanganda position dan shift ni ayirib natijani decrypted_pos nomli o`zgaruvchiga yuklash
+
+# TODO 5 encrypt nomli funksiyada loopdan tashqarida ya'ni yuqorida decrypted_word nomli bo`sh string yaratish
+
+# TODO 5 4-banddan keyin 5-bandda yaratgan stringga alphabet nomli listning decrypted_pos da turgan elementini qo`shib chiqish
+
+# STAGE 3 ASK AND GIVE DIRECTION
+# TODO First Funksiyalarga directionning qiymatiga qarab murojaat qilish!
+*/
