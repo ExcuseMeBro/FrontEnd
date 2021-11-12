8c6c2f6af593775b6ec61127ed0a4acb27fcc618
Arash4567 <shomoyxhacker007@mail.ru>
2021-11-12 07:11:24 +0300

    jQuery 2

---
+++ jQuery 13/index.js
@@ -0,0 +1,69 @@
+$(document).ready(function (){
+    // blur() event 
+    /* $("input").blur(function () { 
+        alert("Hello world!");
+        //TODO: Inputdagi ma`lumotni olib alertga chiqaring
+    }); */
+
+    // change() event 
+    /* $("input").change(function () { 
+        // alert("Hello world!");
+        console.log($("input").val());
+        //TODO: Inputdagi ma`lumotni olib Yuqori registerda yozib beruvchi dastur tuzing
+    }); */
+
+    // click() event 
+    /* $(".btn").click(function () { 
+        alert($("input").val());
+        // console.log($("input").val());
+        //TODO: Inputdagi ma`lumotni olib Yuqori registerda yozib beruvchi dastur tuzing
+    }); */
+
+    // dbclick() event 
+    /* $(".btn").dblclick(function () { 
+        alert($("input").val());
+        // console.log($("input").val());
+    }); */
+
+    // focus() event 
+    // $("input").focus(function () { 
+    //     $("span").css("display", "inline").fadeOut(2000);
+    // });
+
+    // focusin() and focusout() events 
+    /* $("input").focusin(function () { 
+        $(".table").css("background-color", "green");
+    }); 
+    $("input").focusout(function () { 
+        $(".table").css("background-color", "red");
+    }); */
+
+    // hover() event
+    /* $(".table").hover(function () {
+        $(this).css("background", "red");
+    }, function () {
+        $(this).css("background", "green");
+    }); */
+
+    // keydown() event
+    /* $("input").keydown(function () { 
+        $(".table").text($(this).val());
+    }); */
+
+    // keypress() event
+    /* $("input").keypress(function () { 
+        $(".table").text($(this).val());
+    }); */
+
+    // keyup() event
+    /* $("input").keyup(function () { 
+        $(".table").text($(this).val());
+    }); */
+
+    // load() events
+    /* $(".btn").click(function (){
+        $(".loadFile").load("index.html");
+    }); */
+
+    
+});
