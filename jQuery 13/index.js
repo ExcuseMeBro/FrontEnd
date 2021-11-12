cfc7e026e8efebd2cae892de0d960e3f9e7e93c4
Arash4567 <shomoyxhacker007@mail.ru>
2021-11-12 11:38:04 +0300

    jQuery Events

--- jQuery 13/index.js
+++ jQuery 13/index.js
@@ -1,69 +1,97 @@
-$(document).ready(function (){
-    // blur() event 
-    /* $("input").blur(function () { 
+$(document).ready(function () {
+  //1. blur() event
+  /* $("input").blur(function () { 
         alert("Hello world!");
         //TODO: Inputdagi ma`lumotni olib alertga chiqaring
     }); */
-
-    // change() event 
-    /* $("input").change(function () { 
+  //2. change() event
+  /* $("input").change(function () { 
         // alert("Hello world!");
         console.log($("input").val());
         //TODO: Inputdagi ma`lumotni olib Yuqori registerda yozib beruvchi dastur tuzing
     }); */
-
-    // click() event 
-    /* $(".btn").click(function () { 
+  //3. click() event
+  /* $(".btn").click(function () { 
         alert($("input").val());
         // console.log($("input").val());
         //TODO: Inputdagi ma`lumotni olib Yuqori registerda yozib beruvchi dastur tuzing
     }); */
-
-    // dbclick() event 
-    /* $(".btn").dblclick(function () { 
+  //4. dbclick() event
+  /* $(".btn").dblclick(function () { 
         alert($("input").val());
         // console.log($("input").val());
     }); */
-
-    // focus() event 
-    // $("input").focus(function () { 
-    //     $("span").css("display", "inline").fadeOut(2000);
-    // });
-
-    // focusin() and focusout() events 
-    /* $("input").focusin(function () { 
+  //5. focus() event
+  // $("input").focus(function () {
+  //     $("span").css("display", "inline").fadeOut(2000);
+  // });
+  //6. focusin() and focusout() events
+  /* $("input").focusin(function () { 
         $(".table").css("background-color", "green");
     }); 
     $("input").focusout(function () { 
         $(".table").css("background-color", "red");
     }); */
-
-    // hover() event
-    /* $(".table").hover(function () {
+  //7. hover() event
+  /* $(".table").hover(function () {
         $(this).css("background", "red");
     }, function () {
         $(this).css("background", "green");
     }); */
-
-    // keydown() event
-    /* $("input").keydown(function () { 
+  //8. keydown() event
+  /* $("input").keydown(function () { 
         $(".table").text($(this).val());
     }); */
-
-    // keypress() event
-    /* $("input").keypress(function () { 
+  //9. keypress() event
+  /* $("input").keypress(function () { 
         $(".table").text($(this).val());
     }); */
-
-    // keyup() event
-    /* $("input").keyup(function () { 
+  //10. keyup() event
+  /* $("input").keyup(function () { 
         $(".table").text($(this).val());
     }); */
-
-    // load() events
-    /* $(".btn").click(function (){
+  //11. load() events
+  /* $(".btn").click(function (){
         $(".loadFile").load("index.html");
     }); */
-
-    
+  //12. after() event
+  /* $("button").click(function (){
+        $(".table").after('<h1 style="color: red">hello Wolrd!</h1>');
+    }); */
+  //13. mouseup() and mousedown() events
+  /* $('.table').mouseup(function (){
+        $(this).after('<h1 style="color: red">hello Wolrd!</h1>');
+    });
+    $('.table').mousedown(function (){
+        $(this).after('<h1 style="color: green">hello Wolrd!</h1>');
+    }); */
+  //14. mouseenter() event
+  /* $('.table').mouseenter(function (){
+        $(this).after('<h1 style="color: green">hello Wolrd!</h1>');
+    }); */
+  //15. mouseleave() event
+  /* $('.table').mouseleave(function (){
+        $(this).after('<h1 style="color: red">hello Wolrd!</h1>');
+    }); */
+  //16. mousemove() event
+  /* $(document).mousemove(function (event){
+        // console.log(event);
+        $('.table').text("X = " + event.pageX + " - Y = " + event.pageY);
+        $('.table').css("width", event.pageX);
+        $('.table').css("height", event.pageX);
+    }); */
+  //17. mouseover() event {CSSdagi hover effectini jQuery ko`rinishi}
+  /* $(".table").mouseover(function () {
+    $(this).css("width", "250px");
+    $(this).css("height", "250px");
+  });
+  $(".table").mouseleave(function () {
+    $(this).css("width", "150px");
+    $(this).css("height", "150px");
+  }); */
+  
+  //18. on() event
+  /* $(".table").on("click", function () {
+    $(this).css("background", "red");
+  }); */
 });
