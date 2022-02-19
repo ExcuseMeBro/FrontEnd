4e6d60ae6de2a345b630164715b1d0de2db18cdb
Ogabek <shomoyxhacker007@mail.ru>
2022-02-19 14:46:46 +0500

    jQuery 1

---
+++ JavaScript/jQuery 11/app.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    // document.getElementById('info').textContent = "Hello";
+    // $('CSS selector yoziladi')
+    // $('#info').text("Hello");
+    // console.log($('#info').text());
+    // $('#info').css('font-weight', 'bold');
+});
